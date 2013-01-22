b4833da2716745f0f1fb8be49f41938e6e2c19e5
Stefano <acerbetti@gmail.com>
2013-01-22 11:49:13 -0800

    added color category

---
+++ ACEToolKit/UIColor+ACEToolKit.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ACEToolKit.h
+//  ACEToolKitDemo
+//
+//  Created by Stefano Acerbetti on 1/22/13.
+//  Copyright (c) 2013 Stefano Acerbetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ACEToolKit)
+
++ (UIColor *)colorWithHex:(int)hex;
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
+@end
---
+++ ACEToolKit/UIColor+ACEToolKit.m
@@ -0,0 +1,72 @@
+//
+//  UIColor+ACEToolKit.m
+//  ACEToolKitDemo
+//
+//  Created by Stefano Acerbetti on 1/22/13.
+//  Copyright (c) 2013 Stefano Acerbetti. All rights reserved.
+//
+
+#import "UIColor+ACEToolKit.h"
+
+CGFloat colorComponentFrom(NSString *string, NSUInteger start, NSUInteger length)
+{
+    NSString *substring = [string substringWithRange:NSMakeRange(start, length)];
+    NSString *fullHex = length == 2 ? substring : [NSString stringWithFormat: @"%@%@", substring, substring];
+    
+    unsigned hexComponent;
+    [[NSScanner scannerWithString: fullHex] scanHexInt: &hexComponent];
+    return hexComponent / 255.0;
+}
+
+#pragma mark -
+
+@implementation UIColor (ACEToolKit)
+
++ (UIColor *)colorWithHex:(int)hex
+{
+    return [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16)) / 255.0
+                           green:((float)((hex & 0xFF00) >> 8)) / 255.0
+                            blue:((float)(hex & 0xFF)) / 255.0 alpha:1.0];
+}
+
++ (UIColor *)colorWithHexString:(NSString *)hexString
+{
+    CGFloat alpha, red, blue, green;
+    
+    NSString *colorString = [[hexString stringByReplacingOccurrencesOfString:@"#" withString:@""] uppercaseString];
+    switch ([colorString length]) {
+        case 3: // #RGB
+            alpha = 1.0f;
+            red   = colorComponentFrom(colorString, 0, 1);
+            green = colorComponentFrom(colorString, 1, 1);
+            blue  = colorComponentFrom(colorString, 2, 1);
+            break;
+            
+        case 4: // #ARGB
+            alpha = colorComponentFrom(colorString, 0, 1);
+            red   = colorComponentFrom(colorString, 1, 1);
+            green = colorComponentFrom(colorString, 2, 1);
+            blue  = colorComponentFrom(colorString, 3, 1);
+            break;
+            
+        case 6: // #RRGGBB
+            alpha = 1.0f;
+            red   = colorComponentFrom(colorString, 0, 2);
+            green = colorComponentFrom(colorString, 2, 2);
+            blue  = colorComponentFrom(colorString, 4, 2);
+            break;
+            
+        case 8: // #AARRGGBB
+            alpha = colorComponentFrom(colorString, 0, 2);
+            red   = colorComponentFrom(colorString, 2, 2);
+            green = colorComponentFrom(colorString, 4, 2);
+            blue  = colorComponentFrom(colorString, 6, 2);
+            break;
+            
+        default:
+            return nil;
+    }
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
+@end
