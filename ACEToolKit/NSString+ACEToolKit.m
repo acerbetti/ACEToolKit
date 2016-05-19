f8a6709bb6aa45cb7b2cc4b362ab786c76d3d396
Stefano Acerbetti <acerbetti@gmail.com>
2016-05-18 18:48:14 -0700

    added NSString category

---
+++ ACEToolKit/NSString+ACEToolKit.m
@@ -0,0 +1,13 @@
+//
+//  NSString+ACEToolKit.m
+//  ACEToolKitDemo
+//
+//  Created by Stefano Acerbetti on 5/18/16.
+//  Copyright © 2016 Stefano Acerbetti. All rights reserved.
+//
+
+#import "NSString+ACEToolKit.h"
+
+@implementation NSString (ACEToolKit)
+
+@end
